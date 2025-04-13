12cf5719a6e35eb92f4a58bdfe03207466f789b7
plscallMeAlex <alexkung007@gmail.com>
2025-04-13 17:59:01 +0700

    add models

---
+++ server/Models/Permission.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace server.Models
+{
+    public class Permission
+    {
+        public string Id { get; set; } = String.Empty;
+        public string PermissionName { get; set; } = String.Empty;
+        public bool IsReadable { get; set; } = false;
+        public bool IsWritable { get; set; } = false;
+        public bool IsDeletable { get; set; } = false;
+    }
+}
---
+++ server/Models/User.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace server.Models
+{
+    public class User
+    {
+        public string Id { get; set; } = String.Empty;
+        public string FirstName { get; set; } = String.Empty;
+        public string LastName { get; set; } = String.Empty;
+        public string Email { get; set; } = String.Empty;
+        public string? Phone { get; set; }
+        public string UserName { get; set; } = String.Empty;
+        public string Password { get; set; } = String.Empty;
+        
+        public string? RoleId { get; set; }
+        public Role? Role { get; set; } = null;
+
+        public string? PermissionId { get; set; }
+        public Permission? Permission { get; set; } = null;
+    }
+}
