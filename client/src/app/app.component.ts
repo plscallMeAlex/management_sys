67285315c98b2abdcbc8202fb502da861d24705a
plscallMeAlex <alexkung007@gmail.com>
2025-04-14 09:20:15 +0700

    add/sidebar

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SidebarComponent],
   template: `
-    <h1>Hello, {{ title }}</h1>
+    <app-sidebar></app-sidebar>
     <router-outlet/>
     `,
   styles: []
--- client/src/app/components/sidebar/sidebar.component.ts
+++ client/src/app/components/sidebar/sidebar.component.ts
@@ -1,15 +1,60 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { NgFor } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink, RouterLinkActive, NgFor, MatIconModule],
   template: `
-    <p>
-      sidebar works!
-    </p>
+    <div class="w-64 h-screen bg-white border-r border-gray-200">
+      <div class="p-8">
+        <h1 class="text-lg font-bold">YOURLOGO</h1>
+      </div>
+      <div class="flex flex-col">
+      <nav >
+        <a 
+          *ngFor="let item of navItems" 
+          [routerLink]="item.route"
+          routerLinkActive="bg-blue-50 border-l-4 border-blue-500"
+          class="flex items-center px-6 py-3 text-gray-600 hover:bg-blue-50"
+        >
+          <span class="mr-3 text-gray-500">
+            <mat-icon>{{item.icon}}</mat-icon>
+          </span>
+          <span>{{ item.label }}</span>
+        </a>
+      </nav>
+      <nav class="mt-8">
+        <a 
+          *ngFor="let item of navItems2" 
+          [routerLink]="item.route"
+          routerLinkActive="bg-blue-50 border-l-4 border-blue-500"
+          class="flex items-center px-6 py-3 text-gray-600 hover:bg-blue-50"
+        >
+          <span class="mr-3 text-gray-500">
+            <mat-icon>{{item.icon}}</mat-icon>
+          </span>
+          <span>{{ item.label }}</span>
+        </a>
+      </nav>
+      </div>
+    </div>
   `,
   styles: ``
 })
 export class SidebarComponent {
-
+  navItems = [
+    { label: 'Dashboard', icon: 'dashboard', route: '/dashboard' },
+    { label: 'Users', icon: 'group', route: '/users' },
+    { label: 'Documents', icon: 'description', route: '/documents' },
+    { label: 'Photos', icon: 'photo', route: '/photos' },
+    { label: 'Hierarchy', icon: 'account_tree', route: '/hierarchy' },
+  ];
+  navItems2 = [
+    { label: 'Message', icon: 'message', route: '/messages' },
+    { label: 'Help' , icon: 'help', route: '/help' },
+    { label: 'Setting', icon: 'settings', route: '/settings' }
+  ]
 }
