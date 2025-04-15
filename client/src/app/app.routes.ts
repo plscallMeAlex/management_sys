e1d64dbc9ffcaa6041fa6b3e20715204f8cb5d39
plscallMeAlex <alexkung007@gmail.com>
2025-04-16 01:30:36 +0700

    add document route

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { DocumentpgComponent } from './pages/documentpg/documentpg.component';
 
 export const routes: Routes = [
   {
     path: 'dashboard',
     component: DashboardComponent,
+  },
+  {
+    path: 'documents',
+    component: DocumentpgComponent,
   },
   {
     path: '',
---
+++ client/src/app/pages/documentpg/documentpg.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-documentpg',
+  standalone: true,
+  imports: [MatIconModule],
+  template: ` <p>documentpg works!</p> `,
+  styles: ``,
+})
+export class DocumentpgComponent {}
