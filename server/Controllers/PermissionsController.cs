48ee7511227c6ba2050ed6b4c41e745b82f9ba21
plscallMeAlex <alexkung007@gmail.com>
2025-04-15 19:13:49 +0700

    add additional controller

---
+++ server/Controllers/PermissionsController.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using server.Data;
+using AutoMapper;
+using server.Dtos;
+using Microsoft.EntityFrameworkCore;
+
+namespace server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PermissionsController : ControllerBase
+    {
+        private readonly ApplicationDBContext _context;
+        private readonly IMapper _mapper;
+
+        public PermissionsController(ApplicationDBContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        // GET: api/permissions
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<PermissionDto>>> GetPermissions()
+        {
+            var permissions = await _context.Permissions.ToListAsync();
+            var permissionDtos = _mapper.Map<List<PermissionDto>>(permissions);
+            return Ok(permissionDtos);
+        }
+    }
+}
